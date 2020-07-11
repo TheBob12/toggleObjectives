e7e3f90497d09946f2a2a5e8a9f10749a1a2d161
HP <41843643+TheBob12@users.noreply.github.com>
2020-07-11 20:28:16 +0200

    Update HideObjectives.lua

--- HideObjectives/HideObjectives.lua
+++ HideObjectives/HideObjectives.lua
@@ -1,65 +1,48 @@
-
-local tObjF = CreateFrame("Frame", "tObjFrame")
-local MSG_PREFIX = "tObj"
-local success = C_ChatInfo.RegisterAddonMessagePrefix(MSG_PREFIX)
-
-local inDungeon = false
-
-tObjFrame:RegisterEvent("CHAT_MSG_ADDON")
+tObjFrame:RegisterEvent("ZONE_CHANGED")
+tObjFrame:RegisterEvent("ZONE_CHANGED_INDOORS")
 tObjFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-tObjFrame:RegisterEvent("ADDON_LOADED")
-tObjFrame:RegisterEvent("CHALLENGE_MODE_START")
-tObjFrame:RegisterEvent("CHALLENGE_MODE_COMPLETED")
-
-function tObjFrame:CHALLENGE_MODE_START(event,...)
-	inDungeon = true
-	toggle()
-end
-function tObjFrame:CHALLENGE_MODE_COMPLETED(event,...)
-	inDungeon = false
-	toggle()
-end
-
 
 
 tObjFrame:SetScript("OnEvent", function(self, event_name, ...)
-	if self[event_name] then
+	--[[if self[event_name] then
 		return self[event_name](self, event_name, ...)
-	end
+	end--]]
+	
+    local currentMapID = C_Map.GetBestMapForUnit("player") or WorldMapFrame:GetMapID()
+    local currentMapInfo = C_Map.GetMapInfo(currentMapID)
+    local currentMapName = currentMapInfo.name
+    
+    local questList = C_QuestLog.GetQuestsOnMap(currentMapID)
+    local questListID = {}
+    
+    for k,v in pairs(questList) do 
+        
+        if questList[k].questID ~= nil then
+            questListID[questList[k].questID] = true
+            
+        end
+    end
+    
+    
+    local dontHide = false
+    
+    for i=1,GetNumQuestLogEntries() do
+        local title, _, _, isHeader, _, _, _, questID, _, _, IsOnMap = GetQuestLogTitle(i);
+        
+        if isHeader then 
+            dontHide = ((title) == (currentMapName))
+        end
+        
+        if dontHide then
+            if IsQuestWatched(i) ~= true then QuestMapQuestOptions_TrackQuest(questID) end
+        else
+            
+            if IsQuestWatched(i) and questListID[questID] == nil then QuestObjectiveTracker_UntrackQuest(nil, questID); end
+            if IsQuestWatched(i) ~= true and questListID[questID] then QuestMapQuestOptions_TrackQuest(questID); end 
+        end
+        
+    end
+	
 end)
 
-function toggle()
-	local is = tObjDB.frameVisible
-	
-	if is then 
-		if not inDungeon then
-			ObjectiveTrackerFrame:Hide() 
-		else 
-			ObjectiveTrackerFrame:Show() 
-			for i = GetNumQuestWatches(), 1, -1 do 
-				RemoveQuestWatch(GetQuestIndexForWatch(i)) 
-			end
-		end
-	else 
-		ObjectiveTrackerFrame:Show() 
-	end
-end
 
-function tObjFrame:ADDON_LOADED(event,addon)
-	
-	if not tObjDB then
-		tObjDB = {
-			frameVisible = true
-		}
-	else
-		toggle()
-	end
-	
-end
-
-SLASH_TOBJ1 = "/tobj"
-
-SlashCmdList["TOBJ"] = function(msg,editBox)
-	tObjDB.frameVisible = not tObjDB.frameVisible
-	toggle()
-end
