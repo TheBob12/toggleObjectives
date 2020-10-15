4602a22c347620ec58a0fc62afc1719e3f2b4d72
HP <41843643+TheBob12@users.noreply.github.com>
2020-10-15 20:30:52 +0200

    Update HideObjectives.lua
    Fix for SL.

--- HideObjectives/HideObjectives.lua
+++ HideObjectives/HideObjectives.lua
@@ -5,11 +5,7 @@ tObjFrame:RegisterEvent("ZONE_CHANGED")
 tObjFrame:RegisterEvent("ZONE_CHANGED_INDOORS")
 tObjFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 
-
 tObjFrame:SetScript("OnEvent", function(self, event_name, ...)
-	--[[if self[event_name] then
-		return self[event_name](self, event_name, ...)
-	end--]]
 	
     local currentMapID = C_Map.GetBestMapForUnit("player") or WorldMapFrame:GetMapID()
     local currentMapInfo = C_Map.GetMapInfo(currentMapID)
@@ -19,33 +15,33 @@ tObjFrame:SetScript("OnEvent", function(self, event_name, ...)
     local questListID = {}
     
     for k,v in pairs(questList) do 
-        
         if questList[k].questID ~= nil then
             questListID[questList[k].questID] = true
-            
         end
     end
     
-    
     local dontHide = false
     
-    for i=1,GetNumQuestLogEntries() do
-        local title, _, _, isHeader, _, _, _, questID, _, _, IsOnMap = GetQuestLogTitle(i);
-        
+    for i=1,C_QuestLog.GetNumQuestLogEntries() do
+        local title = C_QuestLog.GetTitleForLogIndex(i);
+        local questID = C_QuestLog.GetQuestIDForLogIndex(i) 
+        local isHeader = C_QuestLog.GetTitleForLogIndex(i) == 0 and true or false
+        local IsQuestWatched = C_QuestLog.GetQuestWatchType(questID)
+
+        print(IsQuestWatched)
+
         if isHeader then 
             dontHide = ((title) == (currentMapName))
         end
         
         if dontHide then
-            if IsQuestWatched(i) ~= true then QuestMapQuestOptions_TrackQuest(questID) end
+            if IsQuestWatched ~= true then QuestMapQuestOptions_TrackQuest(questID) end
         else
             
-            if IsQuestWatched(i) and questListID[questID] == nil then QuestObjectiveTracker_UntrackQuest(nil, questID); end
-            if IsQuestWatched(i) ~= true and questListID[questID] then QuestMapQuestOptions_TrackQuest(questID); end 
+            if IsQuestWatched and questListID[questID] == nil then QuestObjectiveTracker_UntrackQuest(nil, questID); end
+            if IsQuestWatched ~= true and questListID[questID] then QuestMapQuestOptions_TrackQuest(questID); end 
         end
         
     end
 	
 end)
-
-
